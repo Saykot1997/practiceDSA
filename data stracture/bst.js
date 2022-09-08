f55dc6ea267316190bab1d96f031a33d9cd53afe
saykot hossain <saykothossain14@gmail.com>
2022-09-08 16:11:38 +0600

    binary search tree add

--- data stracture/bst.js
+++ data stracture/bst.js
@@ -63,6 +63,142 @@ class BST {
             }
         }
     }
+
+    min() {
+        if (!this.root) {
+            return null
+        }
+        let current = this.root
+        while (current.left) {
+            current = current.left
+        }
+        return current.data
+    }
+    max() {
+        if (!this.root) {
+            return null
+        }
+        let current = this.root
+        while (current.right) {
+            current = current.right
+        }
+        return current.data
+    }
+
+    inorder(node = this.root) {
+        let elements = []
+        // travarse left sub tree
+        if (node.left) {
+            elements = elements.concat(this.inorder(node.left))
+        }
+        // travarse base tree
+        elements.push(node.data)
+        // travarse right sub tree
+        if (node.right) {
+            elements = elements.concat(this.inorder(node.right))
+        }
+        return elements
+    }
+
+
+    // Performs preorder traversal of a tree   
+    preorder(node = this.root) {
+        let elements = []
+        // travarse base tree
+        elements.push(node.data)
+        // travarse left sub tree
+        if (node.left) {
+            elements = elements.concat(this.preorder(node.left))
+        }
+        // travarse right sub tree
+        if (node.right) {
+            elements = elements.concat(this.preorder(node.right))
+        }
+        return elements
+    }
+
+
+    // Performs postorder traversal of a tree
+    postorder(node = this.root) {
+        let elements = []
+        // travarse left sub tree
+        if (node.left) {
+            elements = elements.concat(this.postorder(node.left))
+        }
+        // travarse right sub tree
+        if (node.right) {
+            elements = elements.concat(this.postorder(node.right))
+        }
+        // travarse base tree
+        elements.push(node.data)
+        return elements
+    }
+
+    delete(data) {
+        this.root = this.removeNode(this.root, data);
+    }
+
+    removeNode(node, key) {
+        // if the root is null then tree is
+        // empty
+        if (node === null)
+            return null;
+
+        // if data to be delete is less than
+        // roots data then move to left subtree
+        else if (key < node.data) {
+            node.left = this.removeNode(node.left, key);
+            return node;
+        }
+
+        // if data to be delete is greater than
+        // roots data then move to right subtree
+        else if (key > node.data) {
+            node.right = this.removeNode(node.right, key);
+            return node;
+        }
+
+        // if data is similar to the root's data
+        // then delete this node
+        else {
+            // deleting node with no children
+            if (node.left === null && node.right === null) {
+                node = null;
+                return node;
+            }
+
+            // deleting node with one children
+            if (node.left === null) {
+                node = node.right;
+                return node;
+            }
+
+            else if (node.right === null) {
+                node = node.left;
+                return node;
+            }
+
+            // Deleting node with two children
+            // minimum node of the right subtree
+            // is stored in aux
+            let aux = this.findMinNode(node.right);
+            node.data = aux.data;
+
+            node.right = this.removeNode(node.right, aux.data);
+            return node;
+        }
+    }
+
+    //  finds the minimum node in tree
+    // searching starts from given node
+    findMinNode(node) {
+        // if left of a node is null
+        // then it must be minimum node
+        if (node.left === null)
+            return node;
+        else
+            return this.findMinNode(node.left);
+    }
 }
 
 const bst = new BST()
@@ -77,6 +213,14 @@ bst.add(10)
 bst.add(17)
 bst.add(20)
 
+// console.log(bst.max())
+
 // console.log(bst.tree())
 
-console.log(bst.contains(20))
+// console.log(bst.contains(20))
+// console.log(bst.tree())
+// bst.delete(12)
+console.log(bst.tree())
+// console.log(bst.inorder())
+// console.log(bst.preorder())
+console.log(bst.postorder())
