c250bffb847f21e234e2469bf52e4db3362d2d83
saykot hossain <saykothossain14@gmail.com>
2022-09-19 22:37:55 +0600

    graph data stacture add

--- data stracture/graph.js
+++ data stracture/graph.js
@@ -70,6 +70,31 @@ class Graph {
             }
         }
     }
+
+
+    // Main DFS method
+    dfs(startingNode) {
+
+        var visited = {};
+
+        this.DFSUtil(startingNode, visited);
+    }
+
+    // Recursive function which process and explore
+    // all the adjacent vertex of the vertex with which it is called
+    DFSUtil(vert, visited) {
+        visited[vert] = true;
+        console.log(vert);
+
+        var get_neighbours = this.AdjList.get(vert);
+
+        for (var i in get_neighbours) {
+            var get_elem = get_neighbours[i];
+            if (!visited[get_elem])
+                this.DFSUtil(get_elem, visited);
+        }
+    }
+
 }
 
 
@@ -101,4 +126,6 @@ g.addEdge('C', 'F');
 // E -> A D F C
 // F -> E C
 // g.printGraph();
-g.bfs("E")
+// g.bfs("E")
+
+g.dfs("A")
