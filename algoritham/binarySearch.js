9b39bb55b5961ba58c6e61ae690314b7dc67509c
saykot hossain <saykothossain14@gmail.com>
2022-09-19 22:31:43 +0600

    binary serch algorithom add

--- algoritham/binarySearch.js
+++ algoritham/binarySearch.js
@@ -2,25 +2,36 @@
 // the target element is not found.
 
 
-const binarySearch = (array, n) => {
 
+const binarySearch = (array, n) => {
     if (!array.length > 0) {
         return -1
     }
-    if (array[Math.ceil((array.length - 1) / 2)] === n) {
-        return Math.ceil((array.length - 1) / 2)
-    } else if (array[Math.ceil((array.length - 1) / 2)] > n) {
-        let newArray = array.slice(0, Math.ceil((array.length - 1) / 2))
-        return binarySearch(newArray, n)
-    } else {
-        let newArray = array.slice(Math.ceil((array.length - 1) / 2), array.length - 1)
-        return binarySearch(newArray, n)
+
+    let leftPointer = 0
+    let rightPoinert = array.length - 1
+
+    while (leftPointer <= rightPoinert) {
+        let midPointer = Math.floor((leftPointer + rightPoinert) / 2);
+        if (array[leftPointer] === n) {
+            return leftPointer
+        } else if (array[rightPoinert] === n) {
+            return rightPoinert
+        } else if (array[midPointer] === n) {
+            return midPointer
+        } else {
+            if (n > array[midPointer]) {
+                leftPointer = midPointer + 1
+            } else {
+                rightPoinert = midPointer - 1
+            }
+        }
     }
+    return -1
 }
 
-// console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
-// console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5))
-// console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11))
-// console.log(binarySearch([], 3))
-
-// binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8)
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5))
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11))
+console.log(binarySearch([], 3))
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8))
